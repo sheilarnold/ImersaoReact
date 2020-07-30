2e8bc6d7435f00c408478887f3a47da2e857fc7f
Sheila C.S Arnold <62268999+sheilarnold@users.noreply.github.com>
2020-07-29 23:10:22 -0300

    Iniciando com formulários

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -1,32 +1,88 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../Components/PageDefault';
 
 function CadastroCategoria() {
-  return (
-    <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+    const [categorias, setCategorias] = useState(['teste']);
+    
+    const valores = {
+        nome: 'Cat',
+        descricao: 'Cat Cat dogao',
+        cor: '#000',
+    }
 
-      <form>
+    let [values, setValues] = useState(valores);
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-          />
-        </label>
+    return (
+        <PageDefault>
+        <h1>Cadastro de Categoria: {values.nome}</h1>
 
-        <button>
-          Cadastrar
-        </button>
-      </form>
+        <form style={{background: values.cor}} onSubmit={function ahndleSubmit(infoEvento){
+            infoEvento.preventDefault();
+            setCategorias([
+                ...categorias,
+                values
+            ]);
+        }}>
+            <div>
+                <label>
+                Nome da Categoria:
+                <input
+                    type="text"
+                    value={values.nome}
+                    onChange={function cadCategoria(infoEvento){
+                            setValues(infoEvento.target.value)
+                        }
+                    }
+                />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Descrição:
+                    <input
+                        type="text"
+                        value={values.descricao}
+                        onChange={function cadCategoria(infoEvento){
+                                setValues(infoEvento.target.value)
+                            }
+                        }
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Cor:
+                    <input
+                        type="color"
+                        value={values.cor}
+                        onChange={function cadCategoria(infoEvento){
+                                setValues(infoEvento.target.value)
+                            }
+                        }
+                    />
+                </label>
+            </div>
+            <button>
+            Cadastrar
+            </button>
+        </form>
 
+        <ul>
+            {categorias.map((categoria, indice) => {
+                return(
+                    <li key={`${categoria}${indice}`}>
+                        {categoria}
+                    </li>
+                )
+            })}
+        </ul>
 
-      <Link to="/">
-        Ir para home
-      </Link>
-    </PageDefault>
-  )
+        <Link to="/">
+            Ir para home
+        </Link>
+        </PageDefault>
+    )
 }
 
 export default CadastroCategoria;
